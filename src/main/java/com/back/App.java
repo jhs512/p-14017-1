bd30d4401f22c60440aad2f7a6bd7ac8464601a6
jhs512 <jangka512@gmail.com>
2025-05-29 15:35:43 +0900

    step 3, b

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -2,9 +2,6 @@ package com.back;
 
 public class App {
     public int plus(int num1, int num2) {
-        if (num1 == 10 && num2 == 20) return 30;
-        if (num1 == 0 && num2 == 5) return 5;
-
-        return 3;
+        return num1 + num2;
     }
 }
